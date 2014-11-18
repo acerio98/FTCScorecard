1725a4ba936878942542e7d70a06b9af6b88de2c
acerio98 <a.c.riopelle@me.com>
2014-11-18 14:22:14 -0500

    getting ready for reverse

--- app/src/main/java/com/ashburnrobotics/aperture/team/ftcscore/MainActivity.java
+++ app/src/main/java/com/ashburnrobotics/aperture/team/ftcscore/MainActivity.java
@@ -10,21 +10,13 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
-<<<<<<< HEAD
 import com.parse.Parse;
 import com.parse.ParseObject;
-
-public class MainActivity extends Activity implements View.OnClickListener{
-ImageButton addScore;
-private Score myScore;
-=======
 import android.widget.TextView;
-
 
 public class MainActivity extends Activity implements View.OnClickListener{
 ImageButton addScore;
 private TextView text;
->>>>>>> 9d4a884fc520250283ce5cfb1d825ac40968a9cf
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,20 +24,12 @@ private TextView text;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         addScore = (ImageButton)findViewById(R.id.addButton);
-<<<<<<< HEAD
-        addScore.setOnClickListener(this);
-        myScore = new Score();
-        int c = 4;
-        int x = 4;
         ParseObject testObject = new ParseObject("TestObject");
         testObject.put("foo", "bar"); // -- Sends Parse object to cloud
         testObject.saveInBackground();
-=======
         text = (TextView)findViewById(R.id.textView);
         addScore.setOnClickListener(this);
 
-<<<<<<< HEAD
-=======
         if(savedInstanceState == null){
             getFragmentManager().beginTransaction()
                     .add(R.id.mainLayout, new AutonomousFragment())
